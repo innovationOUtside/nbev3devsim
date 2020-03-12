19b0b6e199a7837525e02fb3354d4294dd037aa1
Tony Hirst <tony.hirst@gmail.com>
2020-03-12 17:44:42 +0000

    Make a start on adding resource files to package

--- setup.py
+++ setup.py
@@ -6,7 +6,10 @@ setup(
     version='0.0.1',
     include_package_data=True,
     package_data = {
-        'progs' : ['progs/*.py']},
+        'progs' : ['progs/*.py'],
+        'js' : ['js/*.js'],
+        'css': ['css/*.css'],
+        'images': ['images/*']},
     install_requires=[
         'jp_proxy_widget',
         'nest_asyncio',
